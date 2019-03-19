c279c2c59443a465020de6a2c8a994f161ce6ba1
chamb <chamb@Kewl-PC>
2019-03-18 22:05:26 -0500

    Listing functions

--- src/mathpuzzlesolver/MathPuzzleSolver.java
+++ src/mathpuzzlesolver/MathPuzzleSolver.java
@@ -2,6 +2,48 @@ package mathpuzzlesolver;
 
 public class MathPuzzleSolver {
     public static void main(String[] args) {
+        /*
+        abs(a)
+        acos(a)
+        asin(a)
+        atan(a)
+        cbrt(a)
+        ceil(a)
+        cos(a)
+        cosh(a)
+        exp(a)
+        expml(a)
+        floor(a)
+        getExponent(a)
+        incrementExact(a)
+        log(a)
+        log(10)
+        log1p(a)
+        negateExact(a)
+        signum(a)
+        sin(a)
+        sinh(a)
+        sqrt(a)
+        tan(a)
+        tanh(a)
+        toDegrees(a)
+        toRadians(a)
+        ulp(a)
+        
+        atan2(a, b)
+        floorDiv(a, b)
+        floorMod(a, b)
+        hypot(a, b)
+        IEEEremainder(a, b)
+        max(a, b)
+        min(a, b)
+        nextAfter(a, b)
+        pow(a, b)
+        scalb(a, b)
+        
+        d ss d v v d v v
+        */
+        
         
     }
 }
