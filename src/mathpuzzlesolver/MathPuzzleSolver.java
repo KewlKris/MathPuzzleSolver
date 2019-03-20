9e54ad707543dff06d5eb874cbed9f620e4f0e81
chamb <chamb@Kewl-PC>
2019-03-20 16:45:21 -0500

    Removed comment block

--- src/mathpuzzlesolver/MathPuzzleSolver.java
+++ src/mathpuzzlesolver/MathPuzzleSolver.java
@@ -6,53 +6,6 @@ import java.util.ArrayList;
 
 public class MathPuzzleSolver {
     public static void main(String[] args) {
-        /*
-        abs(a)
-        acos(a)
-        asin(a)
-        atan(a)
-        cbrt(a)
-        ceil(a)
-        cos(a)
-        cosh(a)
-        exp(a)
-        expml(a)
-        floor(a)
-        getExponent(a)
-        incrementExact(a)
-        log(a)
-        log(10)
-        log1p(a)
-        negateExact(a)
-        signum(a)
-        sin(a)
-        sinh(a)
-        sqrt(a)
-        tan(a)
-        tanh(a)
-        toDegrees(a)
-        toRadians(a)
-        ulp(a)
-        
-        Combinators 14
-        + - * /
-        atan2(a, b)
-        floorDiv(a, b)
-        floorMod(a, b)
-        hypot(a, b)
-        IEEEremainder(a, b)
-        max(a, b)
-        min(a, b)
-        nextAfter(a, b)
-        pow(a, b)
-        scalb(a, b)
-        
-        +++2019
-        ++2+019
-        ++20+19
-        +2+0+19
-        */
-        
         char[][] solvingForms = new char[][]
         {
             "xyzabcd".toCharArray(),
