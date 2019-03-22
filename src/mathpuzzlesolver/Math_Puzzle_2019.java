2b8df5c64e21954d7d3093448c078e88655de4b0
Christopher <Christopher@10.7.136.132>
2019-03-22 09:50:39 -0500

    Project ending message

--- src/mathpuzzlesolver/Math_Puzzle_2019.java
+++ src/mathpuzzlesolver/Math_Puzzle_2019.java
@@ -2,6 +2,16 @@ package mathpuzzlesolver;
 
 public class Math_Puzzle_2019 {
     public static void main(String[] args) {
+        /*
+        [][][][][][][][][][][][][][][][][]
+        []                              []
+        []   Explanation of how values  []
+        []     were generated below     []
+        []                              []
+        [][][][][][][][][][][][][][][][][]
+         - Chris
+        */
+        
         System.out.println((int)((((2 + 9) * 0) + 1)));
         System.out.println((int)((((9 + 1) * 0) + 2)));
         System.out.println((int)((((9 * 0) + 2) + 1)));
@@ -102,5 +112,46 @@ public class Math_Puzzle_2019 {
         System.out.println((int)(Math.pow((Math.cbrt((Math.sin(1) + 0)) + 9), 2)));
         System.out.println((int)((Math.pow((Math.cos(0) + 9), 2) - 1)));
         System.out.println((int)(Math.pow(((9 + 1) + 0), 2)));
+        
+        /*
+        
+            Rather than find all 100 solutions by hand, I wanted to write
+        a program that created the equations for me. Knowledge from CS UIL
+        helped me figure out how this program woudl work, namely, using prefix
+        notation to represent mathematical equations.
+        
+        Put simply, writing an equation in prefix notation means you write the operator
+        first:
+        
+        1 + 2           becomes         +12
+        (1 + 2) + 3     becomes         ++123
+        1 + (2 + 3)     becomes         +1+23
+        
+            This is a more "linear" form of writing equations, where values can easily
+        be substituted and added. My program would "brute-force" all possible
+        combinations of an equation of given length, and solve. In order to make
+        sure my solutions were clean and didn't include too many repetitive
+        operations, each "correct" equation would be scored. If an equation did
+        not meet or exceed the current best score calculated, it was not recorded.
+        I gave different operations different weights based on how diffuclt
+        I thought it was to do in your head. In addition to that, the more an
+        operation is used in an equation, the "heavier" it becomes so that the
+        program favors simple, diverse equations.
+        
+            After all of this, another program (Organizer.java) sorted through
+        all simple, solved values and picked the equation of shortest length.
+        This simple, shortest equation becomes the equation in my Excl spreadsheet.
+        
+            The programs I wrote to generate and sort through the equations were
+        written hastily. They are not well documented and many parts of it are
+        "hard-coded" or inefficiently written. Regardless, I'm proud to have written it.
+        Everyone else found their 100 answers over several days. I found mine
+        in several minutes.
+        
+        - Chris
+        
+        */
+        
+        
     }
 }
